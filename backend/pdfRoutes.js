c3a51438a146713ea95f4148ae2f84fe182f1aae
Vishal Gejge <92996047+vishalgejge@users.noreply.github.com>
2024-05-21 21:03:39 +0530

    Update pdfRoutes.js

--- backend/pdfRoutes.js
+++ backend/pdfRoutes.js
@@ -1,13 +1,11 @@
-// pdfRoutes.js
 const express = require('express');
 const { createPdf, fetchPdf, sendPdf } = require('./pdfController');
 const pdfRoute = express.Router();
 
-pdfRoute.post('/createPdf', createPdf); // to generate pdf 
-pdfRoute.get('/fetchPdf', fetchPdf); // to fetch the generated pdf
-pdfRoute.post('/sendPdf', sendPdf); // send pdf to mail
+pdfRoute.post('/createPdf', createPdf);
+pdfRoute.get('/fetchPdf', fetchPdf);
+pdfRoute.post('/sendPdf', sendPdf);
 
-// Dummy route to test backend connection
 pdfRoute.get('/dummyJson', (req, res) => {
     res.json({
         message: "Backend is connected!",
