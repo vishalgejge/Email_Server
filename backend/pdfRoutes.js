34f00bf451fc74a216fbad7e270affa410dba9e7
Vishal Gejge <92996047+vishalgejge@users.noreply.github.com>
2024-05-21 17:35:00 +0530

    Update pdfRoutes.js

--- backend/pdfRoutes.js
+++ backend/pdfRoutes.js
@@ -1,11 +1,9 @@
-const express = require('express')
-const { createPdf, fetchPdf, sendPdf } = require('./pdfController')
-const pdfRoute = express.Router()
+const express = require('express');
+const { createPdf, fetchPdf, sendPdf } = require('./pdfController');
+const pdfRoute = express.Router();
 
-pdfRoute.post('/createPdf',createPdf) // to generate pdf 
+pdfRoute.post('/createPdf', createPdf); // to generate pdf 
+pdfRoute.get('/fetchPdf', fetchPdf); // to fetch the generated pdf
+pdfRoute.post('/sendPdf', sendPdf); // send pdf to mail
 
-pdfRoute.get('/fetchPdf',fetchPdf) // to fetch the generated pdf
-
-pdfRoute.post('/sendPdf',sendPdf) //sent pdf to mail 
-
-module.exports = pdfRoute
+module.exports = pdfRoute;
