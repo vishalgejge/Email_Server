56e9d22fab5ff4b2f06d79f13707235c8a417540
Vishal Gejge <92996047+vishalgejge@users.noreply.github.com>
2024-05-21 17:13:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors({
-    origin: ["https://emailsender-eight.vercel.app"],
+    origin: [""],
     methods: ["POST", "GET"],
     credentials: true
 }));
