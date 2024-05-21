1dea6cfe35e0eee1447e370d0984507bf1d0b79a
Vishal Gejge <92996047+vishalgejge@users.noreply.github.com>
2024-05-21 17:06:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,10 @@ const app = express();
 const env = require('dotenv');
 const cors = require('cors');
 const pdfRoute = require('./pdfRoutes');
-const path = require('path');
 
 env.config();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors({
@@ -19,5 +18,5 @@ app.use(cors({
 app.use('/api/pdf', pdfRoute);
 
 app.listen(port, () => {
-    console.log(`Server running on ${port}`);
+    console.log(`Server running on port ${port}`);
 });
